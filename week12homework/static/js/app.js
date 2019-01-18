00d967cc57b5422924fa9ffab4e374ed250b10a4
shortpoet <soriano.carlitos@gmail.com>
2019-01-18 14:53:26 -0600

    added autocomplete search

--- week12homework/static/js/app.js
+++ week12homework/static/js/app.js
@@ -20,7 +20,8 @@ shapeArr.forEach(x => {
 var states = data.map(x => x.state);
 var stateSet = new Set(states);
 var stateArr = [...stateSet]
-stateArr.forEach(x => {
+var stateSorted = stateArr.sort((a,b) => a > b ? 1 : a === b ? 0 : -1);
+stateSorted.forEach(x => {
     d3.select('#stateSelect').append('option').text(x);
 })
 
@@ -40,6 +41,105 @@ var binSorted = binArr.sort((a,b) => a > b ? 1 : a === b ? 0 : -1);
 binSorted.forEach(x => {
     d3.select('#binSelect').append('option').text(x);
 })
+
+function autocomplete(inp, arr) {
+    /*the autocomplete function takes two arguments,
+    the text field element and an array of possible autocompleted values:*/
+    var currentFocus;
+    /*execute a function when someone writes in the text field:*/
+    inp.addEventListener("input", function(e) {
+        var a, b, i, val = this.value;
+        /*close any already open lists of autocompleted values*/
+        closeAllLists();
+        if (!val) { return false;}
+        currentFocus = -1;
+        /*create a DIV element that will contain the items (values):*/
+        a = document.createElement("DIV");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+        /*append the DIV element as a child of the autocomplete container:*/
+        this.parentNode.appendChild(a);
+        /*for each item in the array...*/
+        for (i = 0; i < arr.length; i++) {
+          /*check if the item starts with the same letters as the text field value:*/
+          if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+            /*create a DIV element for each matching element:*/
+            b = document.createElement("DIV");
+            /*make the matching letters bold:*/
+            b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+            b.innerHTML += arr[i].substr(val.length);
+            /*insert a input field that will hold the current array item's value:*/
+            b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+            /*execute a function when someone clicks on the item value (DIV element):*/
+                b.addEventListener("click", function(e) {
+                /*insert the value for the autocomplete text field:*/
+                inp.value = this.getElementsByTagName("input")[0].value;
+                /*close the list of autocompleted values,
+                (or any other open lists of autocompleted values:*/
+                closeAllLists();
+            });
+            a.appendChild(b);
+          }
+        }
+    });
+    /*execute a function presses a key on the keyboard:*/
+    inp.addEventListener("keydown", function(e) {
+        var x = document.getElementById(this.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+        if (e.keyCode == 40) {
+          /*If the arrow DOWN key is pressed,
+          increase the currentFocus variable:*/
+          currentFocus++;
+          /*and and make the current item more visible:*/
+          addActive(x);
+        } else if (e.keyCode == 38) { //up
+          /*If the arrow UP key is pressed,
+          decrease the currentFocus variable:*/
+          currentFocus--;
+          /*and and make the current item more visible:*/
+          addActive(x);
+        } else if (e.keyCode == 13) {
+          /*If the ENTER key is pressed, prevent the form from being submitted,*/
+          e.preventDefault();
+          if (currentFocus > -1) {
+            /*and simulate a click on the "active" item:*/
+            if (x) x[currentFocus].click();
+          }
+        }
+    });
+    function addActive(x) {
+      /*a function to classify an item as "active":*/
+      if (!x) return false;
+      /*start by removing the "active" class on all items:*/
+      removeActive(x);
+      if (currentFocus >= x.length) currentFocus = 0;
+      if (currentFocus < 0) currentFocus = (x.length - 1);
+      /*add class "autocomplete-active":*/
+      x[currentFocus].classList.add("autocomplete-active");
+    }
+    function removeActive(x) {
+      /*a function to remove the "active" class from all autocomplete items:*/
+      for (var i = 0; i < x.length; i++) {
+        x[i].classList.remove("autocomplete-active");
+      }
+    }
+    function closeAllLists(elmnt) {
+      /*close all autocomplete lists in the document,
+      except the one passed as an argument:*/
+      var x = document.getElementsByClassName("autocomplete-items");
+      for (var i = 0; i < x.length; i++) {
+        if (elmnt != x[i] && elmnt != inp) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
+  /*execute a function when someone clicks in the document:*/
+  document.addEventListener("click", function (e) {
+      closeAllLists(e.target);
+  });
+  }
+autocomplete(document.getElementById('citySearch'), citySorted);
+autocomplete(document.getElementById('stateSearch'), stateSorted);
 
 submit.on("click", function() {
     d3.event.preventDefault();
@@ -90,14 +190,17 @@ submit.on("click", function() {
     console.log(`shapeFilter: `);
     console.log(shapeFilter);
     
+    var citySearchElement = d3.select("#citySearch");
+    var citySearchValue = citySearchElement.property("value")
     var cityValue = d3.select('#citySelect').selectAll("option").filter(function(){return this.selected});
     var cityValArr = cityValue._groups[0].map(x => x.value)
     console.log(`cityValArr: `);
     console.log(cityValArr);
     var cityFilterF = function(cityValArr) {
-        if (cityValArr.length === 0) {
+        if (cityValArr.length === 0 && citySearchValue === "") {
             return ufos;
         } else {
+            cityValArr.push(citySearchValue);
             return data.filter(ufo => cityValArr.reduce((a,b) => a || b === ufo.city, false));
         }
     }
@@ -105,14 +208,17 @@ submit.on("click", function() {
     console.log(`cityFilter: `);
     console.log(cityFilter);
 
+    var stateSearchElement = d3.select("#stateSearch");
+    var stateSearchValue = stateSearchElement.property("value")
     var stateValue = d3.select('#stateSelect').selectAll("option").filter(function(){return this.selected});
-    var stateValArr = stateValue._groups[0].map(x => x.value)
+    var stateValArr = stateValue._groups[0].map(x => x.value);
     console.log(`stateValArr: `);
     console.log(stateValArr);
     var stateFilterF = function(stateValArr) {
-        if (stateValArr.length === 0) {
+        if (stateValArr.length === 0 && stateSearchValue === "") {
             return ufos;
         } else {
+            stateValArr.push(stateSearchValue)
             return data.filter(ufo => stateValArr.reduce((a,b) => a || b === ufo.state, false));
         }
     }
